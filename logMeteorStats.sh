d5b5ce061168851320cc499eef99854f4a98868f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-02-14 23:25:01 +0000

    cd to $here to ensure code available

--- logMeteorStats.sh
+++ logMeteorStats.sh
@@ -2,5 +2,5 @@
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vRMS/bin/activate
-
+cd $here
 python $here/sendToMQTT.py
--- logPiStats.sh
+++ logPiStats.sh
@@ -3,6 +3,8 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vRMS/bin/activate
 
+cd $here
+
 tm=$(vcgencmd measure_temp | cut -d= -f2)
 ds=$(df -h . | tail -1 | awk -F" " '{print $5 }')
 
