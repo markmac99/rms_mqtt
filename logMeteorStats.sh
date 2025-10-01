770eb8028de9294e8ffbb3a47a3dae81f84383c8
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-10-01 23:54:04 +0100

    Adding statid to the other stats

--- logMeteorStats.sh
+++ logMeteorStats.sh
@@ -3,4 +3,4 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vRMS/bin/activate
 cd $here
-python -c "from sendToMQTT import sendToMqtt;sendToMqtt()"
+python -c "from sendToMQTT import sendToMqtt;sendToMqtt(statid='$1')"
--- logPiStats.sh
+++ logPiStats.sh
@@ -5,7 +5,4 @@ source ~/vRMS/bin/activate
 
 cd $here
 
-tm=$(vcgencmd measure_temp | cut -d= -f2)
-ds=$(df -h . | tail -1 | awk -F" " '{print $5 }')
-
-python -c "from sendToMQTT import sendOtherData ; sendOtherData(\"${tm}\",\"${ds}\") ; "
+python -c "from sendToMQTT import sendOtherData;sendOtherData(statid='$1');"
