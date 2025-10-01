605728f704737f26eafd315e5e187b46b556b606
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-10-01 23:55:53 +0100

    remove unused file

--- logPiStats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source ~/vRMS/bin/activate
-
-cd $here
-
-python -c "from sendToMQTT import sendOtherData;sendOtherData(statid='$1');"
